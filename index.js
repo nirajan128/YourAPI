a66d325621fc550bf1f1c5b4621add77d2738765
Nirajan <nirajanshrestha672@gmail.com>
2024-09-11 17:39:41 -0400

    Form designed for submitting data

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 const app = express()
-const port = 3000;
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.render('index.ejs')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-
